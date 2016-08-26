02c8d1eff51dfc16bb65c261c44cb62c9e7852e0
Matthews <T0015236@leeds.gov.uk>
2016-08-26 16:19:27 +0100

    bumped version to 1.2.0

--- server.js
+++ server.js
@@ -79,7 +79,6 @@ app.use(function (req, res, next) {
 
 // Add variables that are available in all views
 app.use(function (req, res, next) {
-  res.locals.serviceName = config.serviceName
   res.locals.cookieText = config.cookieText
   res.locals.releaseVersion = 'v' + releaseVersion
   next()
