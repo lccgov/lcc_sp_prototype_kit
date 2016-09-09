b1439d616f18898ba15436422ff3da046205d28e
rodmiley <rodmiley5@hotmail.com>
2016-09-09 15:21:42 +0100

    Added includes and sass

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -121,6 +121,24 @@
     });
 
 
+     $('.expandContent h3 ~ div').hide();
+
+         $(".expandContent h3").on("click", function()
+        {
+		    $(this).toggleClass("active");
+            var p = $(this).next('div').slideToggle();				
+		    if ($(this).children().find('#tooltip').text() === "Click to expand")
+		    {
+		        $(this).children().find('#tooltip').text('Click to hide')
+		    }
+		    else
+		    {
+		        $(this).children().find('#tooltip').text('Click to expand')
+		    }
+		    return false;
+		}); 
+
+
 
     
 }(jQuery));
