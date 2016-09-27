84d4b5aa3b984fc97bbe57a0e2f5ad43c3020b4a
rodmiley <rodmiley5@hotmail.com>
2016-09-27 15:52:16 +0100

    Added Active leeds prototype

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,24 @@
 (function ($) {
+
+
+      jQuery('ul.date-list').each(function () {
+      var LiN = jQuery(this).find('li').length;
+      if (LiN > 3) {
+      jQuery('li', this).eq(2).nextAll().hide().addClass('toggleable');
+      jQuery(this).append('<a class="plusMinus">Show more...</a>');
+      }
+      });
+      jQuery('ul.date-list').on('click', '.plusMinus', function () {
+      if (jQuery(this).hasClass('active')) {
+      jQuery(this).text('Show more...').removeClass('active');
+      } else {
+      jQuery(this).text('Show less...').addClass('active');
+      }
+      jQuery(this).siblings('li.toggleable').slideToggle();
+      });
+
+
+
     
     //equal heights
 
@@ -173,9 +193,8 @@
     return {
         activate: activateResponsiveDesign
     }
+    
 
 
     
 }(jQuery));
-
-  
