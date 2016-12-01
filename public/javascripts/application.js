1c93cbf0a309159d4b80a90c8f55d67ae4d859a7
Rod Miley <rodmiley5@hotmail.com>
2016-12-01 09:39:39 +0000

    got latest frontent toolkit and templates

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,1 @@
-$(document).ready(function() {
-    var s = $(".masterhead");
-    var pos = s.position();                    
-    $(window).scroll(function() {
-        var windowpos = $(window).scrollTop()>150;
-        if (windowpos >= pos.top) {
-            s.addClass("stick");
-        } else {
-            s.removeClass("stick"); 
-        }
-    });
-});
+
